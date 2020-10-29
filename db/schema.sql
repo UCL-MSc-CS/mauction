ab5b8ca30231bc5df8fe4e80b425cefc9e765dcd
mattShorvon <73358381+mattShorvon@users.noreply.github.com>
2020-10-29 17:22:49 +0000

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -11,8 +11,8 @@ registrationDate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 firstName VARCHAR(45) NULL,
 lastName VARCHAR(45) NULL,
 password VARCHAR(40),
-PRIMARY KEY (buyerID)
-);
+PRIMARY KEY (buyerID))
+ENGINE = InnoDB;
 
 CREATE TABLE `sellers` (
 sellerID INT NOT NULL AUTO_INCREMENT,
@@ -23,21 +23,27 @@ registrationDate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 firstName VARCHAR(45) NULL,
 lastName VARCHAR(45) NULL,
 password VARCHAR(40),
-PRIMARY KEY (sellerID)
-);
+PRIMARY KEY (sellerID))
+ENGINE = InnoDB;
 
 CREATE TABLE `auctions` (
 saleItemID INT NOT NULL AUTO_INCREMENT,
-sellerID INT NOT NULL,
-PRIMARY KEY (saleItemID)
-);
+sellerID INT NOT NULL, startDate DATE NOT NULL,
+startTime TIME NOT NULL, category VARCHAR(100) NOT NULL,
+description VARCHAR(300) NOT NULL, reservePrice DECIMAL NOT NULL,
+endTime TIME NOT NULL, endDate DATE NOT NULL, 
+finalPrice DECIMAL NOT NULL, commission DECIMAL NOT NULL, 
+delivery VARCHAR(20) NOT NULL, outcome VARCHAR(30) NOT NULL,
+cond VARCHAR(30) NOT NULL, 
+PRIMARY KEY (saleItemID))
+ENGINE = InnoDB; 
 
 CREATE TABLE `bids` (
 bidID INT NOT NULL AUTO_INCREMENT,
 buyerID INT NOT NULL,
 saleItemID INT NOT NULL,
-PRIMARY KEY (bidID)
-);
+PRIMARY KEY (bidID))
+ENGINE = InnoDB;
 
 INSERT INTO `buyers` (userName, email, address, firstName, lastName, password)
 VALUES ("cecrandell", "caroline.crandell.20@ucl.ac.uk", "CA, USA", "Caroline", "Crandell", SHA("password"));
@@ -51,8 +57,8 @@ VALUES ("mattShorvon", "matthew.shorvon.20@ucl.ac.uk", "London, UK", "Matthew", 
 INSERT INTO `sellers` (userName, email, address, firstName, lastName, password)
 VALUES ("AriannaBourke", "arianna.bourke.20@ucl.ac.uk", "London, UK", "Arianna", "Bourke", SHA("1234"));
 
-INSERT INTO `auctions` (sellerID)
-VALUES (1);
+INSERT INTO `auctions` (saleItemID, sellerID, startDate, startTime, category, description, reservePrice, endTime, endDate, finalPrice, commission, delivery, outcome, cond)
+VALUES ('1','1','01-01-20','00:00:00','video games','EPIC fortnite skins','10000.00','12:00:00','10-10-20','10000.00','1','post','still bidding','new');
 
 INSERT INTO `bids` (buyerID, saleItemID)
 VALUES (1,1);
