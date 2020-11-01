010061a9559ea2b7833d07a4b97fa9a3217f2d6b
AriannaBourke <72710987+AriannaBourke@users.noreply.github.com>
2020-11-01 09:25:07 +0000

    Add files via upload

--- register.php
+++ register.php
@@ -1,52 +1,53 @@
+<?php include 'database_con.php'; ?>
+
 <?php include_once("header.php")?>
 
 <div class="container">
 <h2 class="my-3">Register new account</h2>
 
-<!-- Create auction form -->
 <form method="POST" action="process_registration.php">
-  <div class="form-group row">
-    <label for="accountType" class="col-sm-2 col-form-label text-right">Registering as a:</label>
-	<div class="col-sm-10">
-	  <div class="form-check form-check-inline">
-        <input class="form-check-input" type="radio" name="accountType" id="accountBuyer" value="buyer" checked>
-        <label class="form-check-label" for="accountBuyer">Buyer</label>
-      </div>
-      <div class="form-check form-check-inline">
-        <input class="form-check-input" type="radio" name="accountType" id="accountSeller" value="seller">
-        <label class="form-check-label" for="accountSeller">Seller</label>
-      </div>
-      <small id="accountTypeHelp" class="form-text-inline text-muted"><span class="text-danger">* Required.</span></small>
+<div class="form-group row">
+	<label for="username" class="col-sm-2 col-form-label text-right">Username</label>
+		<div class="col-sm-10">
+		<input type="text" class="form-control" id="username" name="username" placeholder="Enter Your username"/>
+	<small id="usernameHelp" name="username" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
 	</div>
-  </div>
-  <div class="form-group row">
-    <label for="email" class="col-sm-2 col-form-label text-right">Email</label>
-	<div class="col-sm-10">
-      <input type="text" class="form-control" id="email" placeholder="Email">
-      <small id="emailHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
+</div>
+<div class="form-group row">
+	<label for="firstName" class="col-sm-2 col-form-label text-right">First Name</label>
+		<div class="col-sm-10">
+		<input type="text" class="form-control" id="firstName" name="firstName" placeholder="Enter Your First Name"/>
+	<small id="firstNameHelp" name="firstName" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
 	</div>
-  </div>
-  <div class="form-group row">
-    <label for="password" class="col-sm-2 col-form-label text-right">Password</label>
-    <div class="col-sm-10">
-      <input type="password" class="form-control" id="password" placeholder="Password">
-      <small id="passwordHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
-    </div>
-  </div>
-  <div class="form-group row">
-    <label for="passwordConfirmation" class="col-sm-2 col-form-label text-right">Repeat password</label>
-    <div class="col-sm-10">
-      <input type="password" class="form-control" id="passwordConfirmation" placeholder="Enter password again">
-      <small id="passwordConfirmationHelp" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
-    </div>
-  </div>
-  <div class="form-group row">
-    <button type="submit" class="btn btn-primary form-control">Register</button>
-  </div>
+</div>
+<div class="form-group row">
+	<label for="lastName" class="col-sm-2 col-form-label text-right">Last Name</label>
+		<div class="col-sm-10">
+		<input type="text" class="form-control" id="lastName" name="lastName" placeholder="Enter Your Last Name"/>
+	<small id="lastNameHelp" name="lastName" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
+	</div>
+</div>
+<div class="form-group row">
+	<label for="email" class="col-sm-2 col-form-label text-right">Email</label>
+		<div class="col-sm-10">
+		<input type="text" class="form-control" id="email" name="email" placeholder="Enter Your email"/>
+	<small id="emailHelp" name="email" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
+	</div>
+</div>
+<div class="form-group row">
+	<label for="password" class="col-sm-2 col-form-label text-right">password</label>
+		<div class="col-sm-10">
+		<input type="text" class="form-control" id="password" name="password" placeholder="Enter Your password"/>
+	<small id="passwordHelp" name="password" class="form-text text-muted"><span class="text-danger">* Required.</span></small>
+	</div>
+</div>
+<div class="form-group row">
+	<input id="show-btn" type="submit" name="submit" class="btn btn-primary form-control" value="Register"/>
+</div>
 </form>
 
 <div class="text-center">Already have an account? <a href="" data-toggle="modal" data-target="#loginModal">Login</a>
 
 </div>
-
 <?php include_once("footer.php")?>
+
