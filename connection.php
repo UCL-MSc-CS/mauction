224630cf8de4ef52953a1b4bc424b83f42d55aae
Caroline Crandell <carolineecrandell@gmail.com>
2020-11-01 02:07:24 -0800

    Added functions to connection.php

--- connection.php
+++ connection.php
@@ -8,10 +8,22 @@ $success = mysqli_real_connect(
    $port
 ); -->
 <?php
+function OpenCon()
+ {
  $host = $_ENV["$host"];
  $user = $_ENV["$user"];
  $password = $_ENV["$password"];
  $db = $_ENV["$db"];
-
-$connection = mysqli_connect($host,$user,$password,$db)or die('Error connecting to MySQL server.' . mysql_error());
+ $conn = new mysqli($host, $user, $password, $db) or die("Connect failed: %s\n". $conn -> error);
+ 
+ return $conn;
+ }
+ 
+function CloseCon($conn)
+ {
+ $conn -> close();
+ }
+   
 ?>
+
+<!-- https://www.cloudways.com/blog/connect-mysql-with-php/#createdatabase -->
