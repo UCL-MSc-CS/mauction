54380f086446f407c372a85a159d49aa4a524191
mattShorvon <73358381+mattShorvon@users.noreply.github.com>
2020-11-04 18:08:03 +0000

    Update create_auction_result.php

--- create_auction_result.php
+++ create_auction_result.php
@@ -46,7 +46,7 @@ if (isset($_POST['submit'])) {
 
             else {
                         echo '<div class="text-center">Auction successfully created! <a href="FIXME">View your new listing.</a></div>';
-                        $query = "INSERT INTO auctions (itemName, startPrice, category, description) VALUES('$auctionTitle','$startPrice','$category','$description')"; 
+                        $query = "INSERT INTO auctions (itemName, startPrice, category, description, endDate) VALUES('$auctionTitle','$startPrice','$category','$description', '$endDate')"; 
 		        if (!mysqli_query($connection, $query)) {die('Error: ' . mysqli_error($connection));}
             } }
 ?>
