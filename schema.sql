56d75b84a685d505ffc8566534bf95e01ccea542
mattShorvon <73358381+mattShorvon@users.noreply.github.com>
2020-11-04 17:58:14 +0000

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -29,19 +29,19 @@ ENGINE = InnoDB;
 CREATE TABLE `auctions` (
 saleItemID INT NOT NULL AUTO_INCREMENT,
 itemName VARCHAR(200) NOT NULL,
-userID INT NOT NULL, 
+userID INT  NULL, 
 startDate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 category VARCHAR(100) NOT NULL,
 startPrice DECIMAL NOT NULL,
-description VARCHAR(300) NOT NULL, 
+description VARCHAR(300)  NULL, 
 reservePrice DECIMAL NULL,
-endTime TIME NOT NULL, 
+endTime TIME NULL, 
 endDate DATETIME NOT NULL, 
 finalPrice DECIMAL NULL, 
-commission DECIMAL NOT NULL,               -- commission is a fixed value, no dependance on finalPrice yet
-delivery VARCHAR(20) NOT NULL, 
-outcome VARCHAR(30) NOT NULL,
-cond VARCHAR(30) NOT NULL, 
+commission DECIMAL NULL,               -- commission is a fixed value, no dependance on finalPrice yet
+delivery VARCHAR(20)  NULL, 
+outcome VARCHAR(30)  NULL,
+cond VARCHAR(30)  NULL, 
 PRIMARY KEY (saleItemID))
 ENGINE = InnoDB; 
              
@@ -55,22 +55,8 @@ bidAmount DECIMAL NOT NULL,
 PRIMARY KEY (bidID))
 ENGINE = InnoDB;
 
-INSERT INTO `buyers` (userName, email, address, firstName, lastName, password)
-VALUES ("cecrandell", "caroline.crandell.20@ucl.ac.uk", "CA, USA", "Caroline", "Crandell", SHA("password"));                                                                                        
 
-INSERT INTO `buyers` (userName, email, address, firstName, lastName, password)
-VALUES ("erinuclkwon", "wei.quan.20@ucl.ac.uk", "Birmingham, UK", "Wei", "Quan", SHA("1234"));
-
-INSERT INTO `sellers` (userName, email, address, firstName, lastName, password)
-VALUES ("mattShorvon", "matthew.shorvon.20@ucl.ac.uk", "London, UK", "Matthew", "Shorvon", SHA("password"));
-
-INSERT INTO `sellers` (userName, email, address, firstName, lastName, password)
-VALUES ("AriannaBourke", "arianna.bourke.20@ucl.ac.uk", "London, UK", "Arianna", "Bourke", SHA("1234"));
-
-INSERT INTO `auctions` (saleItemID, sellerID, startDate, startTime, category, description, reservePrice, endTime, endDate, finalPrice, commission, delivery, outcome, cond)
-VALUES ('1','1','01-01-20','00:00:00','video games','EPIC fortnite skins','10000.00','12:00:00','10-10-20','10000.00','1','post','still bidding','new');
-
-INSERT INTO `bids` (buyerID, saleItemID)
-VALUES (1,1);
+INSERT INTO `auctions` (saleItemID, userID, startDate, category, itemName, description, startPrice, endDate, finalPrice, commission, delivery, outcome, cond)
+VALUES ('1','1','01-01-20 00:00:00','video games','EPIC fortnite skins', 'epic','10000.00','12:00:00 10-10-20','10000.00','1','post','still bidding','new');
 
 
