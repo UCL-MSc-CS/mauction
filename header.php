911262c9f86f2f6fbb876f2e0fcda77e4034fe59
erinuclkwon <72717299+erinuclkwon@users.noreply.github.com>
2020-11-05 00:58:22 +0000

    Add files via upload
    don't know why I get the undefined index :((((((

--- header.php
+++ header.php
@@ -22,7 +22,7 @@
   <!-- Custom CSS file -->
   <link rel="stylesheet" href="css/custom.css">
 
-  <title>MAUCTION</title>
+  <title>[My Auction Site] <!--CHANGEME!--></title>
 </head>
 
 
@@ -30,7 +30,7 @@
 
 <!-- Navbars -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light mx-2">
-  <a class="navbar-brand" href="#">MAUCTION</a>
+  <a class="navbar-brand" href="#">Site Name <!--CHANGEME!--></a>
   <ul class="navbar-nav ml-auto">
     <li class="nav-item">
     
--- login_result.php
+++ login_result.php
@@ -3,8 +3,6 @@
 // TODO: Extract $_POST variables, check they're OK, and attempt to login.
 // Notify user of success/failure and redirect/give navigation options.
 
-//Matt 01/11: this is going to be a bunch of $variable = $_POST[" "] and if..!isset.. echo: statements
-
 // For now, I will just set session variables and redirect.
 
 session_start();
@@ -14,7 +12,7 @@ $_SESSION['account_type'] = "buyer";
 
 echo('<div class="text-center">You are now logged in! You will be redirected shortly.</div>');
 
-// Redirect to index after 5 seconds. //Matt 01/11: the index is currently just the homepage. 
+// Redirect to index after 5 seconds
 header("refresh:5;url=index.php");
 
 ?>
