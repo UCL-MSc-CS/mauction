d107f7103435f42d2925b92fd79489a06a58955f
mattShorvon <73358381+mattShorvon@users.noreply.github.com>
2020-11-08 16:25:19 +0000

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -2,10 +2,17 @@ DROP DATABASE IF EXISTS mauction;
 CREATE DATABASE mauction;
 USE mauction;
 
+-- things to finalise: put in itemPhoto attribute when ready, multivalued attributes, separate buyer and seller entitites? 
+
+-- CREATE DOMAIN MORE_THAN_ZERO AS REAL CHECK
+-- (value > 0);
+
+-- Should create a domain to stop people creating startDates in the past. 
+
 CREATE TABLE `users` (
 userID INT NOT NULL AUTO_INCREMENT,
 userName VARCHAR(45) NOT NULL,
-email VARCHAR(45) NOT NULL,              -- removed addresses as attribute (may put it as separate entity)
+email VARCHAR(45) NOT NULL,              
 registrationDate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 firstName VARCHAR(45) NULL,
 lastName VARCHAR(45) NULL,
@@ -15,8 +22,8 @@ city VARCHAR (45) NULL,
 addressLine1 VARCHAR (80) NULL,
 addressLine2 VARCHAR (80) NULL,
 postcode VARCHAR (20) NULL, 
-password VARCHAR(40) NOT NULL,
-status VARCHAR(20) NOT NULL,
+password VARCHAR(60) NOT NULL,
+accountType VARCHAR(20) NOT NULL,
 PRIMARY KEY (userID))
 ENGINE = InnoDB;
 
@@ -29,19 +36,19 @@ ENGINE = InnoDB;
 CREATE TABLE `auctions` (
 saleItemID INT NOT NULL AUTO_INCREMENT,
 itemName VARCHAR(200) NOT NULL,
-userID INT  NULL, 
+userID INT NOT NULL, 
 startDate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 category VARCHAR(100) NOT NULL,
 startPrice DECIMAL NOT NULL,
-description VARCHAR(300)  NULL, 
+description VARCHAR(300) NOT NULL, 
 reservePrice DECIMAL NULL,
-endTime TIME NULL, 
-endDate DATE NOT NULL,                -- temporarily just a date object
+endTime TIME NOT NULL, 
+endDate DATE NOT NULL, 
 finalPrice DECIMAL NULL, 
-commission DECIMAL NULL,               -- commission is a fixed value, no dependance on finalPrice yet
-delivery VARCHAR(20)  NULL, 
-outcome VARCHAR(30)  NULL,
-cond VARCHAR(30)  NULL, 
+commission DECIMAL NOT NULL,               -- commission is a fixed value, no dependance on finalPrice yet
+delivery VARCHAR(20) NOT NULL, 
+outcome VARCHAR(30) NOT NULL,
+cond VARCHAR(30) NOT NULL, 
 PRIMARY KEY (saleItemID))
 ENGINE = InnoDB; 
              
@@ -55,8 +62,27 @@ bidAmount DECIMAL NOT NULL,
 PRIMARY KEY (bidID))
 ENGINE = InnoDB;
 
+INSERT INTO `users` (userName, email, firstName, lastName, country, password, status)
+VALUES ("cecrandell", "caroline.crandell.20@ucl.ac.uk", "Caroline", "Crandell", "USA", SHA("password"), 'buyer');
 
-INSERT INTO `auctions` (saleItemID, userID, startDate, category, itemName, description, startPrice, endDate, finalPrice, commission, delivery, outcome, cond)
-VALUES ('1','1','01-01-20','video games','EPIC fortnite skins', 'epic','10000.00','10-10-20 12:00:00','10000.00','1','post','still bidding','new');
+INSERT INTO `users` (userName, email, firstName, lastName, country, password, status)
+VALUES ("erinuclkwon", "wei.quan.20@ucl.ac.uk", "Wei", "Quan", "UK", SHA("1234"), 'seller');
 
+INSERT INTO `users` (userName, email, firstName, lastName, country, password, status)
+VALUES ("mattShorvon", "matthew.shorvon.20@ucl.ac.uk", "Matthew", "Shorvon", "UK", SHA("password"), 'seller');
 
+INSERT INTO `users` (userName, email, firstName, lastName, country, password, status)
+VALUES ("AriannaBourke", "arianna.bourke.20@ucl.ac.uk", "Arianna", "Bourke", "UK", SHA("1234"), 'buyer');
+
+             -- have to edit this insert with new attributes. 
+INSERT INTO `auctions` (itemName, userID, category, startPrice, description, reservePrice, endTime, endDate, commission, delivery, outcome, cond)
+VALUES ("Vaccine", 1, "Health", 1000000, "COVID-19 Cure", 10000000, '12:00:00','10-10-21', 20, "Post", "Bidding", "New");
+
+INSERT INTO `auctions` (itemName, userID, category, startPrice, description, reservePrice, endTime, endDate, commission, delivery, outcome, cond)
+VALUES ("Vase", 1, "Home Decor", 1000000, "Perfect for flowers", 10000000, '12:00:00','10-10-21', 20, "Post", "Bidding", "New");
+
+INSERT INTO `auctions` (itemName, userID, category, startPrice, description, reservePrice, endTime, endDate, commission, delivery, outcome, cond)
+VALUES ("New President", 1, "Mental Wellbeing", 1000000, "Harris2020", 10000000, '12:00:00','10-10-21', 20, "Post", "Bidding", "New");
+
+INSERT INTO `bids` (userID, saleItemID, bidAmount)
+VALUES (1,1,4);
