938f35e87d526b14b10e9e018fa2a1634a893da5
mattShorvon <73358381+mattShorvon@users.noreply.github.com>
2020-11-11 16:49:12 +0000

    Update Matts_Schema.sql

--- db/Matts_Schema.sql
+++ db/Matts_Schema.sql
@@ -9,7 +9,7 @@ USE mauction;
 
 -- Should create a domain to stop people creating startDates in the past. 
 
-CREATE TABLE 'users' (
+CREATE TABLE users (
 userID INT NOT NULL AUTO_INCREMENT,
 userName VARCHAR(45) NOT NULL,
 email VARCHAR(45) NOT NULL,              
@@ -27,13 +27,13 @@ accountType VARCHAR(20) NOT NULL,
 PRIMARY KEY (userID))
 ENGINE = InnoDB;
 
-CREATE TABLE 'photos' (
+CREATE TABLE photos (
 saleItemID INT NOT NULL,
 photo VARCHAR(200) NULL,
 PRIMARY KEY (saleItemID))
 ENGINE = InnoDB;
 
-CREATE TABLE 'auctions' (
+CREATE TABLE auctions (
 saleItemID INT NOT NULL AUTO_INCREMENT,
 itemName VARCHAR(200) NOT NULL,
 userID INT NOT NULL, 
@@ -52,7 +52,7 @@ cond VARCHAR(30) NOT NULL,
 PRIMARY KEY (saleItemID))
 ENGINE = InnoDB; 
              
-CREATE TABLE 'bids' (
+CREATE TABLE bids (
 bidID INT NULL AUTO_INCREMENT,
 userID INT NULL,
 bidTime DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
@@ -62,41 +62,41 @@ bidAmount DECIMAL NULL,
 PRIMARY KEY (bidID))
 ENGINE = InnoDB;
 
-INSERT INTO 'users' (userName, email, firstName, lastName, country, password, accountType)
+INSERT INTO users (userName, email, firstName, lastName, country, password, accountType)
 VALUES ("cecrandell", "caroline.crandell.20@ucl.ac.uk", "Caroline", "Crandell", "USA", SHA("password"), 'buyer');
 
-INSERT INTO 'users' (userName, email, firstName, lastName, country, password, accountType)
+INSERT INTO users (userName, email, firstName, lastName, country, password, accountType)
 VALUES ("erinuclkwon", "wei.quan.20@ucl.ac.uk", "Wei", "Quan", "UK", SHA("1234"), 'seller');
 
-INSERT INTO 'users' (userName, email, firstName, lastName, country, password, accountType)
+INSERT INTO users (userName, email, firstName, lastName, country, password, accountType)
 VALUES ("mattShorvon", "matthew.shorvon.20@ucl.ac.uk", "Matthew", "Shorvon", "UK", SHA("password"), 'seller');
 
-INSERT INTO 'users' (userName, email, firstName, lastName, country, password, accountType)
+INSERT INTO users (userName, email, firstName, lastName, country, password, accountType)
 VALUES ("AriannaBourke", "arianna.bourke.20@ucl.ac.uk", "Arianna", "Bourke", "UK", SHA("1234"), 'buyer');
 
              -- have to edit this insert with new attributes. 
-INSERT INTO 'auctions' (itemName, userID, category, startPrice, description, reservePrice, endTime, endDate, commission, delivery, outcome, cond)
+INSERT INTO auctions (itemName, userID, category, startPrice, description, reservePrice, endTime, endDate, commission, delivery, outcome, cond)
 VALUES ("Vaccine", 1, "Health", 1000000, "COVID-19 Cure", 10000000, '12:00:00','10-4-21', 20, "Post", "Bidding", "New");
 
-INSERT INTO 'auctions' (itemName, userID, category, startPrice, description, reservePrice, endTime, endDate, commission, delivery, outcome, cond)
+INSERT INTO auctions (itemName, userID, category, startPrice, description, reservePrice, endTime, endDate, commission, delivery, outcome, cond)
 VALUES ("Vase", 1, "Home Decor", 10, "Perfect for flowers", 10000000, '12:00:00','10-7-21', 1, "Post", "Bidding", "New");
 
-INSERT INTO 'auctions' (itemName, userID, category, startPrice, description, reservePrice, endTime, endDate, commission, delivery, outcome, cond)
+INSERT INTO auctions (itemName, userID, category, startPrice, description, reservePrice, endTime, endDate, commission, delivery, outcome, cond)
 VALUES ("New President", 1, "Mental Wellbeing", 2020, "Harris2020", 10000000, '12:00:00','10-8-21', 2, "Post", "Bidding", "New");
 
 
-INSERT INTO 'auctions' (itemName, userID, category, startPrice, description, reservePrice, endTime, endDate, commission, delivery, outcome, cond)
+INSERT INTO auctions (itemName, userID, category, startPrice, description, reservePrice, endTime, endDate, commission, delivery, outcome, cond)
 VALUES ("Bowl", 1, "Home Decor", 3, "Perfect for ice cream", 10000000, '12:00:00','10-9-21', 3, "Post", "Bidding", "New");
 
-INSERT INTO 'auctions' (itemName, userID, category, startPrice, description, reservePrice, endTime, endDate, commission, delivery, outcome, cond)
+INSERT INTO auctions (itemName, userID, category, startPrice, description, reservePrice, endTime, endDate, commission, delivery, outcome, cond)
 VALUES ("M&Ms", 1, "Mental Wellbeing", 1, "Perfect for post-graduate students", 10000000, '12:00:00','10-10-21', 4, "Post", "Bidding", "New");
 
-INSERT INTO 'auctions' (itemName, userID, category, startPrice, description, reservePrice, endTime, endDate, commission, delivery, outcome, cond)
+INSERT INTO auctions (itemName, userID, category, startPrice, description, reservePrice, endTime, endDate, commission, delivery, outcome, cond)
 VALUES ("Toothpaste", 1, "Health", 10, "For clenched teeth", 10000000, '12:00:00','09-10-21', 5, "Post", "Bidding", "New");
 
-INSERT INTO 'auctions' (itemName, userID, category, startPrice, description, reservePrice, endTime, endDate, commission, delivery, outcome, cond)
+INSERT INTO auctions (itemName, userID, category, startPrice, description, reservePrice, endTime, endDate, commission, delivery, outcome, cond)
 VALUES ("Toothpaste", 1, "Health", 1000000, "For sensitive teeth", 10000000, '12:00:00','09-10-21', 5, "Post", "Bidding", "New");
 
 
-INSERT INTO 'bids' (userID, saleItemID, bidAmount)
+INSERT INTO bids (userID, saleItemID, bidAmount)
 VALUES (1,1,4);
