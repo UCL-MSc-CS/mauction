fac1f2661fec74c4b8dccd7a84cad29e70e65833
Caroline Crandell <carolineecrandell@gmail.com>
2020-11-12 04:32:26 -0800

    No repeat bid results

--- recommendations.php
+++ recommendations.php
@@ -37,7 +37,7 @@ include 'connection.php';
   }
   $noResults = array();
   for ($x = 0; $x <= count($title2) - 1; $x++) {
-    $query3 = "SELECT userID, itemName, description, startPrice, commission, endDate FROM auctions WHERE userID <> '$userID' AND itemName LIKE '$title2[$x]'";
+    $query3 = "SELECT userID, saleItemID, itemName, description, startPrice, commission, endDate FROM auctions WHERE userID <> '$userID' AND itemName LIKE '$title2[$x]'";
     $result3 = mysqli_query($connection, $query3) or die('Error making select users query' . mysql_error());
     $queryRes3 = mysqli_num_rows($result3);
     if ($queryRes3 == 0) {
@@ -47,13 +47,16 @@ include 'connection.php';
       // TODO: Loop through results and print them out as list items.
       while ($row3 = mysqli_fetch_assoc($result3)) {
         $item_id = $row3['userID'];
+        $saleItemID = $row3['saleItemID'];
         $title3 = $row3['itemName'];
         $description = $row3['description'];
         $current_price = $row3['startPrice'];
         $num_bids = $row3['commission'];
         $end_date = $row3['endDate'];
         // This uses a function defined in utilities.php
-        print_listing_li($item_id, $title3, $description, $current_price, $num_bids, $end_date);
+        if (!in_array("$saleItemID", $title)) {
+          print_listing_li($item_id, $title3, $description, $current_price, $num_bids, $end_date);
+        }
       }
     }
   }
