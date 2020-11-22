8a55c48c0c6c43224eea08f577b9150acdda7082
Caroline Crandell <carolineecrandell@gmail.com>
2020-11-22 10:28:57 -0800

    Fixed place bid

--- place_bid_result.php
+++ place_bid_result.php
@@ -13,20 +13,23 @@ include 'connection.php';
             
 if (isset($_POST['submit'])) {
             $bidAmount = mysqli_real_escape_string($connection, $_POST['bidAmount']);
-            $bidTime = date("Y-m-d H:i:s");
+            // $bidTime = date("Y-m-d H:i:s");
 	          //$userID = mysqli_query($connection, "SELECT 'userID' FROM users WHERE userName = '$loginusername' ") or die('Error...' . mysqli_error());
             $item_id = $_GET['item_id'];
+            $userName = $_SESSION['username'];
             
 
             if (empty($bidAmount)) {
-                        echo "Please provide a bid amount";}  //Matt 01/11: this checks that something is in bidAmount
+                        echo "Please provide a bid amount";
+                    }  //Matt 01/11: this checks that something is in bidAmount
             $query = "SELECT MAX(bidAmount) FROM bid WHERE saleItemID = '$item_id' ";
             $max_bid = mysqli_query($connection,$query) or die('Error making maxBid query' . mysqli_error($connection));
-            if ($bidAmount <= $max_bid) {
+            $queryResult = mysqli_num_rows($max_bid);
+            if ($bidAmount <= $queryResult) {
                 echo "Please enter an amount above the current leading bid";}
             else {
                         echo '<div class="text-center">Bid placed successfully! <a href=index.php>Back to browse.</a></div>';
-                        $query = "INSERT INTO bids (saleItemID, bidAmount, bidTime) VALUES('$item_id','$bidAmount','$bidTime')"; 
+                        $query = "INSERT INTO bid (userName, saleItemID, bidAmount) VALUES('$userName','$item_id','$bidAmount')"; 
 		        if (!mysqli_query($connection, $query)) {
 				die('Error: ' . mysqli_error($connection));}
             } }
