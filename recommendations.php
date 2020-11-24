75bafa42e8e11a4986733b1536da5de5fbc1917a
Caroline Crandell <carolineecrandell@gmail.com>
2020-11-24 15:16:43 -0800

    Recommendations fixed

--- recommendations.php
+++ recommendations.php
@@ -35,21 +35,20 @@ include 'connection.php';
   }
   $noResults = array();
   for ($x = 0; $x <= count($title2) - 1; $x++) {
-    $query3 = "SELECT userName, saleItemID, itemName, description, startPrice, endDate FROM Auction WHERE userName <> '$userName' AND itemName LIKE '$title2[$x]'";
+    $chopped = substr($title2[$x], 0, 3);
+    $query3 = "SELECT userName, saleItemID, itemName, description, startPrice, endDate FROM Auction WHERE userName <> '$userName' AND itemName LIKE '%$chopped%'";
     $result3 = mysqli_query($connection, $query3) or die('Error making select users query' . mysqli_error());
     $queryRes3 = mysqli_num_rows($result3);
     if ($queryRes3 == 0) {
       $noResults . array_push($noResults, "0");
     } else {
       $noResults . array_push($noResults, "1");
-      // TODO: Loop through results and print them out as list items.
       while ($row3 = mysqli_fetch_assoc($result3)) {
         $item_id = $row3['userName'];
         $saleItemID = $row3['saleItemID'];
         $title3 = $row3['itemName'];
         $description = $row3['description'];
         $current_price = $row3['startPrice'];
-        // $num_bids = $row3['commission'];
         $num_bids = 0;
         $end_date = $row3['endDate'];
         for ($y = 0; $y <= count($arr) - 1; $y++) {
