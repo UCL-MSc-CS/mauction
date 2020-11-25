dbdcc300506d59429a62c2800ace5ff27689bcb3
Caroline Crandell <carolineecrandell@gmail.com>
2020-11-25 12:16:28 -0800

    Watchlist works

--- watchlist.php
+++ watchlist.php
@@ -10,14 +10,14 @@ include 'connection.php';
 
     <?php
     $userName = $_SESSION['username'];
-    $bidQuery = "SELECT Auction.*, MAX(bidAmount) as maxBid, COUNT(bidID) as countBid FROM Auction, Bid WHERE Auction.saleItemID = Bid.saleItemID GROUP BY Auction.saleItemID ORDER BY itemName ASC";
+    $bidQuery = "SELECT Auction.*, MAX(bidAmount) as maxBid, COUNT(bidID) as countBid FROM Auction, Bid WHERE Auction.saleItemID = Bid.saleItemID AND Bid.userName = '$userName' GROUP BY Auction.saleItemID ORDER BY itemName ASC";
     $bidResult = mysqli_query($connection, $bidQuery) or die('Error making select users query' . mysqli_error());
     $bidQueryRes = mysqli_num_rows($bidResult);
     $arr = array();
     while ($bidRow = mysqli_fetch_assoc($bidResult)) {
         $arr . array_push($arr, $bidRow);
     }
-    $query = "SELECT Auction.*, Watchlist.* FROM Auction, Watchlist WHERE userName = '$userName' AND Auction.saleItemID = Watchlist.saleItemID";
+    $query = "SELECT Auction.*, Watchlist.* FROM Auction, Watchlist WHERE Watchlist.userName = '$userName' AND Auction.saleItemID = Watchlist.saleItemID";
     $result = mysqli_query($connection, $query) or die('Error making select users query' . mysqli_error());
     $queryRes = mysqli_num_rows($result);
 
@@ -26,6 +26,8 @@ include 'connection.php';
         $title = $row['itemName'];
         $description = $row['description'];
         $end_date = $row['endDate'];
+        $current_price = $row['startPrice'];
+        $num_bids = 0;
         for ($y = 0; $y <= count($arr) - 1; $y++) {
             if ($saleItemID == $arr[$y]['saleItemID']) {
                 $current_price = $arr[$y]['maxBid'];
