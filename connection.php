9e4b3e339382838da5371a5473b7d37721b7894b
Caroline Crandell <carolineecrandell@gmail.com>
2020-11-29 14:45:05 -0800

    JAWS

--- connection.php
+++ connection.php
@@ -13,5 +13,15 @@ $conn = new mysqli($hostname, $username, $password, $database);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-?>
+// 
 
+
+// $servername = "localhost";
+// $username = "root";
+// $password = "root";
+// $database = "mauction";
+
+// // Create connection
+// $connection = mysqli_connect($servername,$username,$password,$database) 
+//     or die('Error connecting to MySQL server.' . mysqli_error());
+// ?>
