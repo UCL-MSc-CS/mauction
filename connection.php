a5c66067088c67780b5a7ae31efb94dce4812007
Caroline Crandell <carolineecrandell@gmail.com>
2020-11-29 13:12:34 -0800

    Updated environment variables

--- connection.php
+++ connection.php
@@ -1,18 +1,12 @@
 <?php
 
-// $dbconfig = parse_ini_file(".env");
+$dbconfig = parse_ini_file(".env");
 
-// $servername = $dbconfig["host"];
-// $username = $dbconfig["user"];
-// $password = $dbconfig["password"];
-// $database = $dbconfig["db"];
+$servername = $dbconfig["host"];
+$username = $dbconfig["user"];
+$password = $dbconfig["password"];
+$database = $dbconfig["db"];
 
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$database = "mauction";
-
-// Create connection
 $connection = mysqli_connect($servername,$username,$password,$database) 
     or die('Error connecting to MySQL server.' . mysqli_error());
 ?>
