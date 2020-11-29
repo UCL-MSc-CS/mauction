b8be78995324645cfda1eee689f5f1b4ef8b6133
Caroline Crandell <carolineecrandell@gmail.com>
2020-11-29 14:03:47 -0800

    Heroku

--- connection.php
+++ connection.php
@@ -1,11 +1,30 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$database = "mauction";
+// $dbconfig = parse_ini_file(".env");
 
-$connection = mysqli_connect($servername,$username,$password,$database) 
-    or die('Error connecting to MySQL server.' . mysqli_error());
+// $servername = $dbconfig["host"];
+// $username = $dbconfig["user"];
+// $password = $dbconfig["password"];
+// $database = $dbconfig["db"];
+
+$url = getenv('JAWSDB_URL');
+$dbparts = parse_url($url);
+
+$hostname = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$database = ltrim($dbparts['path'],'/');
+
+// Create connection
+$conn = new mysqli($hostname, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connection was successfully established!";
+
+// $connection = mysqli_connect($servername,$username,$password,$database) 
+//     or die('Error connecting to MySQL server.' . mysqli_error());
 ?>
 
