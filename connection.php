96d4614d3882e9bf619bce9db6e051ddd76f5793
Caroline Crandell <carolineecrandell@gmail.com>
2020-11-29 14:31:59 -0800

    JAWSDB

--- connection.php
+++ connection.php
@@ -1,12 +1,5 @@
 <?php
 
-// $dbconfig = parse_ini_file(".env");
-
-// $servername = $dbconfig["host"];
-// $username = $dbconfig["user"];
-// $password = $dbconfig["password"];
-// $database = $dbconfig["db"];
-
 $url = getenv('JAWSDB_URL');
 $dbparts = parse_url($url);
 
@@ -15,16 +8,10 @@ $username = $dbparts['user'];
 $password = $dbparts['password'];
 $database = ltrim($dbparts['path'],'/');
 
-// Create connection
 $conn = new mysqli($hostname, $username, $password, $database);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connection was successfully established!";
-
-// $connection = mysqli_connect($servername,$username,$password,$database) 
-//     or die('Error connecting to MySQL server.' . mysqli_error());
 ?>
 
